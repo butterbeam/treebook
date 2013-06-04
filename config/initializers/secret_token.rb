854841cc4aa683e71387130a0a64f4fea6680c8f
Nga Tran <ntran117319@troy.edu>
2013-06-04 09:10:59 -0500

    Butter and Bean

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook::Application.config.secret_token = 'ee5f2cffd60dcc7127166c85fa88c027899802699694cfb1e52e81e8fb89e6bd669532217b3c15978cb7e075f39e1384dec12b7acc2a65005a960672ab79043d'
